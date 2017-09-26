c2c13c4de68465ec4fec9d8f5c5560557dab1c1a
GabrielRubio <gabriel.rubio.usp@gmail.com>
2017-09-26 16:48:14 -0300

    matriz de correlacao

---
+++ MatrizDistancia/matrizCorrelacao.R
@@ -0,0 +1,66 @@
+#pontos de exemplo
+a <- c(1:10)
+b <- a*2
+c <- a*(b/3)
+d <- -b
+
+#matrix de pontos onde cada coluna eh uma coordenada e cada linha eh um ponto
+matrixPontos <- rbind(a,b,c,d)
+
+#funcao que calcula a correlacao 
+distCor <- function(pA, pB){ return( 1 - cor(pA, pB))}
+
+#ajuste de corelacao
+adjust <- function(matrix){
+  d <- dim(matrix)
+  for(i in 1:d[1]){
+    for(j in 1:d[2]){
+      value <- matrix[i,j]
+      if(value<0){
+        matrix[i,j] <- value + 1
+      }else{
+        matrix[i,j] <- 1 - value
+      }
+    }
+  }
+  return(matrix)
+}
+
+distMat <- function(vet){
+  n = dim(vet)
+  m = matrix(0,n[1],n[1])
+  
+  for(i in 1:n[1]){
+    for(j in i:n[1]){
+      m[i,j]<-distCor(vet[i,],vet[j,])
+      m[j,i]<-m[i,j]
+    }
+  }
+  
+  rownames(m)<-rownames(vet)
+  colnames(m)<-rownames(vet)
+  # m <- adjust(m)
+  return(m)
+}
+
+distMat(matrixPontos)
+
+##### Dados reais (wine)
+wine.data <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", sep = ",")
+wine.ann <- wine.data[,1]
+wine.data <- wine.data[,-1]
+
+data = distMat(as.matrix(wine.data))
+data = as.dist(data)
+
+library(cluster)
+wine.pam <- pam(scale(data), 3)
+erro.pam <- sum(!wine.pam$clustering == wine.ann)
+
+wine.fanny <- fanny(scale(data), 3)
+erro.fanny <- sum(!wine.fanny$clustering == wine.ann)
+
+par(mfrow = c(1,3))
+plot(wine.data[,1], wine.data[,7], col = wine.fanny$clustering, main = "Cluster Fanny")
+plot(wine.data[,1], wine.data[,7], col = wine.pam$clustering, main = "Cluster Pam")
+plot(wine.data[,1], wine.data[,7], col = wine.ann, main = "Cluster Real")
