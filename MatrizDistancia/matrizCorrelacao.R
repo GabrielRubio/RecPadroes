3c8dbf38f90fd4b8a4fcc5ef549d15ab72f03591
GabrielRubio <gabriel.rubio.usp@gmail.com>
2017-09-26 17:08:50 -0300

    comentando

--- MatrizDistancia/matrizCorrelacao.R
+++ MatrizDistancia/matrizCorrelacao.R
@@ -10,7 +10,7 @@ matrixPontos <- rbind(a,b,c,d)
 #funcao que calcula a correlacao 
 distCor <- function(pA, pB){ return( 1 - cor(pA, pB))}
 
-#ajuste de corelacao
+#ajuste de correlacao
 adjust <- function(matrix){
   d <- dim(matrix)
   for(i in 1:d[1]){
@@ -26,6 +26,7 @@ adjust <- function(matrix){
   return(matrix)
 }
 
+#calcula a correlacao gerando uma matriz
 distMat <- function(vet){
   n = dim(vet)
   m = matrix(0,n[1],n[1])
@@ -43,6 +44,7 @@ distMat <- function(vet){
   return(m)
 }
 
+# exemplo
 distMat(matrixPontos)
 
 ##### Dados reais (wine)
@@ -50,9 +52,13 @@ wine.data <- read.table("http://archive.ics.uci.edu/ml/machine-learning-database
 wine.ann <- wine.data[,1]
 wine.data <- wine.data[,-1]
 
+#fazendo a matriz de distancia
 data = distMat(as.matrix(wine.data))
+
+#convertendo para um objeto dist
 data = as.dist(data)
 
+#usando os algoritmos de cluster Pam e Fuzzy
 library(cluster)
 wine.pam <- pam(scale(data), 3)
 erro.pam <- sum(!wine.pam$clustering == wine.ann)
@@ -60,7 +66,16 @@ erro.pam <- sum(!wine.pam$clustering == wine.ann)
 wine.fanny <- fanny(scale(data), 3)
 erro.fanny <- sum(!wine.fanny$clustering == wine.ann)
 
+#plotando os graficos com os diferentes cluster e o cluster real
 par(mfrow = c(1,3))
 plot(wine.data[,1], wine.data[,7], col = wine.fanny$clustering, main = "Cluster Fanny")
 plot(wine.data[,1], wine.data[,7], col = wine.pam$clustering, main = "Cluster Pam")
 plot(wine.data[,1], wine.data[,7], col = wine.ann, main = "Cluster Real")
+
+#vendo o erro
+#quantidade de elementos nos grupos Reais
+table(wine.ann)
+#quantidade de elementos nos grupos gerados pelo fanny
+table(wine.fanny$clustering)
+#quantidade de elementos nos grupos gerados pelo pam
+table(wine.pam$clustering)
